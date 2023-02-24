f7560308eaf968e493e64a26a4f6a9f7cc9a1823
simmikukreya <simmikukreya123@gmail.com>
2023-02-24 21:44:58 +0700

    first commit

---
+++ lab7/src/CoursesView.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>รายชื่อวิชาทั้งหมด</h3>
+    <hr />
+    <table>
+      <tr>
+        <th>รหัสวิชา</th>
+        <th>ชื่อวิชา</th>
+      </tr>
+  
+      <tr v-for="(course, index) in courses" :key="index">
+        <td style="text-align: left">{{ course.course_id }}</td>
+        <td style="text-align: left">
+          {{ course.course_name }}
+        </td>
+      </tr>
+    </table>
+</template>
+  
